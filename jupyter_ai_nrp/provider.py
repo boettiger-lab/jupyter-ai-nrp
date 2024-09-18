08584986aa1fb606208546bb08da0c4875ff299f
Dima Mishin <dimm@dimm.dev>
2024-09-17 20:03:12 -0700

    litellm

--- jupyter_ai_nrp/provider.py
+++ jupyter_ai_nrp/provider.py
@@ -9,7 +9,7 @@ class NRPProvider(BaseProvider, OpenAI):
     id = "nrp"
     name = "NRP"
     models = [
-        "meta-llama/Meta-Llama-3.1-70B-Instruct",
+        "llama3",
     ]
     help = "Click here for more details on [NRP](https://docs.nrp.ai/userdocs/ai/llm-managed/)"
     model_id_key = "model_name"
@@ -20,7 +20,7 @@ class NRPProvider(BaseProvider, OpenAI):
         name="NRP_API_KEY", keyword_param="openai_api_key",
     )
     # openai_api_key = getenv("NRP_API_KEY", '')
-    openai_api_base = getenv("NRP_API_BASE", 'https://nrp-llama3-api.nrp-nautilus.io/v1')
+    openai_api_base = getenv("NRP_API_BASE", 'https://litellm.nrp-nautilus.io/v1')
     openai_organization = "NRP"
     persona = Persona(name="NRP", avatar_route="api/ai/static/jupyternaut.svg")
 
@@ -41,7 +41,7 @@ class ChatNRPProvider(BaseProvider, ChatOpenAI):
     id = "nrp-chat"
     name = "NRP"
     models = [
-        "meta-llama/Meta-Llama-3.1-70B-Instruct",
+        "llama3",
     ]
     help = "Click here for more details on [NRP](https://docs.nrp.ai/userdocs/ai/llm-managed/)"
     model_id_key = "model_name"
@@ -51,7 +51,7 @@ class ChatNRPProvider(BaseProvider, ChatOpenAI):
     auth_strategy =  EnvAuthStrategy(
         name="NRP_API_KEY", keyword_param="openai_api_key",
     )
-    openai_api_base = getenv("NRP_API_BASE", 'https://nrp-llama3-api.nrp-nautilus.io/v1')
+    openai_api_base = getenv("NRP_API_BASE", 'https://litellm.nrp-nautilus.io/v1')
     openai_organization = "NRP"
     persona = Persona(name="NRP", avatar_route="api/ai/static/jupyternaut.svg")
 
@@ -72,7 +72,7 @@ class NRPEmbeddingsProvider(BaseEmbeddingsProvider, OpenAIEmbeddings):
     id = "nrp-embeddings"
     name = "NRP"
     models = [
-        "meta-llama/Meta-Llama-3.1-70B-Instruct",
+        "llama3",
     ]
     help = "Click here for more details on [NRP](https://docs.nrp.ai/userdocs/ai/llm-managed/)"
     model_id_key = "model"
@@ -82,6 +82,6 @@ class NRPEmbeddingsProvider(BaseEmbeddingsProvider, OpenAIEmbeddings):
     auth_strategy =  EnvAuthStrategy(
         name="NRP_API_KEY", keyword_param="openai_api_key",
     )
-    openai_api_base = getenv("NRP_API_BASE", 'https://nrp-llama3-api.nrp-nautilus.io/v1')
+    openai_api_base = getenv("NRP_API_BASE", 'https://litellm.nrp-nautilus.io/v1')
     openai_organization = "NRP"
     persona = Persona(name="NRP", avatar_route="api/ai/static/jupyternaut.svg")
